7d611ca03e4efb9bcd4074a320b0763eedab191b
arun <ayogeshwaran@vmware.com>
2020-04-15 23:40:53 +0530

    Injecting SharedPreferences instance from the main.dart class. Making that instance ready to be injected into the MyApp class as SharedPreferences.getInstance() is a async call

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/repositories/data_repository.dart';
 import 'app/services/api.dart';
+import 'app/services/data_cache_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'en_GB';
   await initializeDateFormatting();
-  runApp(MyApp());
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(MyApp(
+    sharedPreferences: sharedPreferences,
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final SharedPreferences sharedPreferences;
+  const MyApp({Key key, @required this.sharedPreferences}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Provider<DataRepository>(
-      create: (_) => DataRepository(apiService: APIService(API.sandbox())),
+      create: (_) => DataRepository(
+        apiService: APIService(API.sandbox()),
+        dataCacheService:
+            DataCacheService(sharedPreferences: sharedPreferences),
+      ),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Coronavirus Tracker',
